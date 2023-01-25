4452f7b4854d96d5760fc9993e6d30745ff6a4aa
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-25 14:15:54 +0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Putut Yusri Bahtiar on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
